f775ee0e2c108cc98f899eddd9d7182fb086cb13
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-01-26 07:59:22 -0300

    Tweaks in the projects
    Little tweaks in the css of the projects to make them look better.

--- projetos/spotify-imersao/script.js
+++ projetos/spotify-imersao/script.js
@@ -30,7 +30,7 @@ const container = document.querySelector(".offer__list-item");
 
 const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
   const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
-  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
+  const numColumns = Math.floor(containerWidth / 230); //número de colunas com base na largura do container
 
   //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
   container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
