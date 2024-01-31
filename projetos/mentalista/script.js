83b2924c38c2935b7629f6d3c19938d78483b7cc
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-01-31 08:59:51 -0300

    Projeto Mentalista
    Evolução do CSS
    Adição de Menu de configurações
    Remoção do alert

--- projetos/mentalista/script.js
+++ projetos/mentalista/script.js
@@ -1,8 +1,16 @@
+
+//variables
 let dificuldade = 100;
-let contador = 6;
+let contador = 7;
 let numeroSecreto = parseInt(Math.random() * dificuldade + 1);
 console.log ('O número secreto é ' + numeroSecreto);
 
+
+//HTML elements
+document.getElementById('instructions').innerHTML = `Chute um número entre 1 e ${dificuldade}`;
+
+
+// Game Code
 let tentativas = 1;
 
 function adivinharNumero() {
@@ -11,19 +19,23 @@ function adivinharNumero() {
 
   if (chute == numeroSecreto) {
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';  
-    alert(`Isso Ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`);
+    document.getElementById('results').innerHTML = `Isso Ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`;
   } else {
       if (chute > numeroSecreto) {
-          alert(`O número secreto é menor que ${chute}. Chutes restantes: ${contador}`);
+        document.getElementById('results').innerHTML = `O número secreto é menor que ${chute}. Chutes restantes: ${contador}`;
       } else {
-          alert(`O número secreto é maior que ${chute}. Chutes restantes: ${contador}`);
+        document.getElementById('results').innerHTML = `O número secreto é maior que ${chute}. Chutes restantes: ${contador}`;
       }
       tentativas++;
   }
 
   if (contador <= 0) {
-      alert(`Poxa vida! Não foi dessa vez. O número secreto era ${numeroSecreto}. Deseja jogar novamente?`);
+    document.getElementById('results').innerHTML = `Poxa vida! Não foi dessa vez. O número secreto era ${numeroSecreto}. Atualize a página jogar novamente.`;
   }
+}
 
-  
+
+//settings
+function settingsMenu () {
+  document.getElementById('settings__menu').classList.toggle('show')
 }
