01c149f56067dbd4adcba598458983a7646dd0e2
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-03-06 08:31:39 -0300

    Projeto Alura Books
    Finalização do JS do Alura Books:
    - Adição de Filtragem de livros
    - metodo sort, map e filter.

---
+++ projetos/alura_books/script/sort.js
@@ -0,0 +1,7 @@
+const btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco() {
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco);
+    exibirOsLivrosNaTela(livrosOrdenados);
+}
