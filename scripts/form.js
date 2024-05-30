8b631067d6708563106b20cca281b2d0174d397b
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-05-30 09:27:03 -0300

    Finalização da lógica do Modal

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,38 @@
-const nome = document.querySelector('.name');
-const email = document.querySelector('.email');
-const message = document.querySelector('.message');
+// Open Forms
+const modalForms = document.querySelector('.contact-modal');
+const openFormBtn = document.getElementById('openform')
 
-addEventListener('submit', e => {
-    e.preventDefault
-    console.log(
-        nome,
-        email,
-        message
-    )
+openFormBtn.addEventListener('click', showForm)
+function showForm(){
+    modalForms.classList.remove("contact-modal-hide");
+}
+
+//Submit and Close Forms
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+const formInput = document.querySelector('.js-form');
+
+formInput.addEventListener('submit', (e) => {
+    e.preventDefault();
+    saveFormData();
+    clearForm();
 })
+
+function saveFormData() {
+    const formContact = {   
+        "name": nameInput.value,
+        "email": emailInput.value,
+        "message": messageInput.value
+    }
+
+    console.log(formContact)
+}
+
+function clearForm() {
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+    modalForms.classList.add("contact-modal-hide");
+}
