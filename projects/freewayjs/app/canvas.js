5122c9e311ee2b43b29eb9b08430560ce384d7bd
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-07-26 08:55:56 -0300

    Update to game
    - Added SCore
    - Added player two movement

--- projects/freewayjs/app/canvas.js
+++ projects/freewayjs/app/canvas.js
@@ -17,11 +17,13 @@ function preload() {
 }
 
 // Players
-let xPlayerOne = 300;
+let xPlayerOne = 260;
 let yPlayerOne = 590;
-let xPlayerTwo = 1100
+let xPlayerTwo = 1076;
 let yPlayerTwo = 590;
 let collision = false;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
 function drawActors(actor, x, y) {
     image(actor, x, y, 60, 50);
@@ -32,26 +34,60 @@ function playerOneMovement() {
         yPlayerOne -= 5;
     } if (keyIsDown(DOWN_ARROW)) {
         yPlayerOne += 5;
+    } if (keyIsDown(87)) {
+        yPlayerTwo -= 5;
+    } if (keyIsDown(83)) {
+        yPlayerTwo += 5;
     }
 }
 
-function cowllision() {
+function checkCowllision(xPlayer,yPlayer) {
     //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
     for (let i = 0; i < carActors.length; i++) {
-        collision = collideRectCircle(xCars[i], yCars[i], carLength, carHeight, xPlayerOne, yPlayerOne, 30)
+        collision = collideRectCircle(xCars[i], yCars[i], carLength, carHeight, xPlayer+30, yPlayer+25, 40)
         if (collision) {
-            print("Crashed!")
+            resetPosition();
+            playerOneScore -= 1;
         }
     }
 }
+
+function resetPosition() {
+    yPlayerOne = 590;    
+}
+
+function drawScoreOne() {
+    textAlign(CENTER);
+    textSize(40);
+    stroke(color(255,200,30));
+    fill(color(255,200,30));
+    text(playerOneScore, width / 5, 43)
+}
+
+function drawScoreTwo() {
+    textAlign(CENTER);
+    textSize(40);
+    stroke(color(255,200,30));
+    fill(color(255,200,30));
+    text(playerTwoScore, width * 0.75, 43)
+}
+
+function setScore() {
+    if (yPlayerOne < 15) {
+        playerOneScore += 1;
+        resetPosition();
+    }
+}
+
+
 
 
 // Cars
 let xCars = [1480, 1480, 1480, 20, 20, 20];
 let yCars = [75, 160, 250, 345, 430, 520];
-let carsSpeed = [2, 5, 7, 3, 8, 10];
-let carHeight = 80;
-let carLength = 50;
+let carsSpeed = [2, 5, 7, 8, 4, 3];
+let carLength = 80;
+let carHeight = 50;
 
 function drawCars() {
     for (
@@ -59,18 +95,14 @@ function drawCars() {
         i < carActors.length;
         i = i + 1
     ) {
-        image(carActors[i], xCars[i], yCars[i], carHeight, carLength);
+        image(carActors[i], xCars[i], yCars[i], carLength, carHeight);
     }
 }
 
 function carMovement() {
-    for (let i = 0; i < xCars.length / 2; i++) {
+    for (let i = 0; i < xCars.length; i++) {
         xCars[i] -= carsSpeed[i];
     };
-
-    for (let i = 2; i < xCars.length; i++) {
-        xCars[i] += carsSpeed[i];
-    }
 }
 
 function outOfCanvas(xCar) {
@@ -104,4 +136,9 @@ function draw() {
     carMovement();
     carInitialPosition();
     playerOneMovement();
+    checkCowllision(xPlayerOne,yPlayerOne);
+    checkCowllision(xPlayerTwo,yPlayerTwo);
+    drawScoreOne();
+    drawScoreTwo();
+    setScore();
 }
